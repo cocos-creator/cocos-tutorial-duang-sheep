bff7d9b1e6f98be2cb1199204fa8685a3b47dac3
knoxHuang <821686174@qq.com>
2015-03-23 18:11:42 +0800

    创建背景与地面

---
+++ duang-sheep/assets/scripts/ScrollPicture.js
@@ -0,0 +1,17 @@
+var ScrollPicture = Fire.extend(Fire.Component);
+
+//-- 滚动的速度
+ScrollPicture.prop('speed', 200);
+
+//-- X轴边缘
+ScrollPicture.prop('offsetX', 0);
+
+//-- 更新
+ScrollPicture.prototype.update = function () {
+    this.entity.transform.x -= Fire.Time.deltaTime * this.speed;
+    if (this.entity.transform.x < -this.offsetX) {
+        this.entity.transform.x = this.entity.transform.x + this.offsetX;
+    }
+};
+
+module.exports = ScrollPicture;
